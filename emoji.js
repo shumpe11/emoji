b7222cd1ee6d11f689ab34a404953aa89c9bdc75
shumpe11 <62501172+shumpe11@users.noreply.github.com>
2020-07-02 00:05:43 +0900

    Add files via upload
    ellipseからemojiへ

--- emoji.js
+++ emoji.js
@@ -1,19 +1,81 @@
-const randomEmoji = function(){
-  let emojiCode = Math.floor( Math.random() * (129685 + 1 - 127744) ) + 127744 ;
+/**
+ * Unicodeで使える絵文字をランダムに返す関数
+ * 127744~129685という数字はUnicodeの絵文字の文字コード
+ * @return {[string]} 文字コードではなく、絵文字そのものを返す
+ */
+const randomizeEmoji = function(){
+  // let emojiCode = Math.floor( Math.random() * (129685 + 1 - 127744) ) + 127744 ;
+  let emojiCode = random(10) > 7.75 ? floor(random(128512, 128592)) : floor(random(127744, 128318));
   let emoji = String.fromCodePoint(emojiCode);
   return emoji;
 }
 
-let circs = [];
-let numCircs = 60;
-let cir;
-function setup() {
-	createCanvas(screen.width,12000);
+//ユーザーからのコマンド操作で動く関数
+const stopEmoji = function(){
+  emojispeed = 0;
+}
+const startEmoji = function(){
+  emojispeed = 4;
+}
+const changePien = function(){
+  let emoji = String.fromCodePoint(129402);
+  emojiObj.emoji = emoji
+}
 
-	for(i=0;i<numCircs;i++){
-		c = new circObj(random(20,220), i ) // generate a random sized circObj and store it's ID for later
+// // スクロール禁止
+// const no_scroll = function(){
+//     // PCでのスクロール禁止
+//     document.addEventListener("mousewheel", scroll_control, { passive: false });
+//     // スマホでのタッチ操作でのスクロール禁止
+//     document.addEventListener("touchmove", scroll_control, { passive: false });
+// }
+// スクロール禁止解除
+// const return_scroll = function(){
+//     // PCでのスクロール禁止解除
+//     document.removeEventListener("mousewheel", scroll_control, { passive: false });
+//     // スマホでのタッチ操作でのスクロール禁止解除
+//     document.removeEventListener('touchmove', scroll_control, { passive: false });
+// }
+
+// // スクロール関連メソッド
+// const scroll_control = function(event){
+//     event.preventDefault();
+// }
+//
+let circs = [];
+let numEmojis = 60;
+let cir;
+let emojispeed = 4;
+let archive = []
+// console.log(window.width)
+// // 1. 変数mqlにMediaQueryListを格納
+// const mql = window.matchMedia('(min-width: 769px)');
+//
+// // 2. メディアクエリに応じて実行したい処理を関数として定義
+// const handleMediaQuery = function(mql) {
+//   if (mql.matches) {
+//     // 769px以上の場合の処理
+//     emojispeed = 4
+//   } else {
+//     // 769px未満の場合の処理
+//     emojispeed = 4;
+//   }
+// };
+
+
+// // 3. イベントリスナーを追加（メディアクエリの条件一致を監視）
+// mql.addListener(handleMediaQuery);
+//
+// // 4. 初回チェックのため関数を一度実行
+// handleMediaQuery(mql);
+function setup() {
+  bg = loadImage('universe.jpg');
+	createCanvas(1100,12000);
+	for(i=0;i<numEmojis;i++){
+		c = new emojiObj(random(10,440), i ) // generate a random sized emojiObj and store it's ID for later
 		circs.push(c); //add it to the array.
 	}
+  // no_scroll();
 }
 
 
@@ -21,36 +83,34 @@ function draw(){
 	background(255);
 
 
-	for(j=0;j<numCircs;j++){
-		circs[j].place(circs); //try to place a circObj on the screen
+	for(j=0;j<numEmojis;j++){
+		circs[j].place(circs); //try to place a emojiObj on the screen
 	}
 
-	for(j=0;j<numCircs;j++){
-		circs[j].y -= 2; //try to place a circObj on the screen
+	for(j=0;j<numEmojis;j++){
+		circs[j].y -= emojispeed; //try to place a emojiObj on the screen
 	}
 
-	for(k=0;k<numCircs;k++){ // display the objects
+	for(k=0;k<numEmojis;k++){ // display the objects
 		circs[k].disp();
 	}
 }
 
-function circObj(d, id){
-	this.x = random(width)
-	this.y = random(height)
+function emojiObj(d,id){
+	this.x = random(width) - d
+	this.y = random(height) - d
 	this.d = d
 	this.id = id;
 	this.color = color(random(255),random(255),random(255))
 	this.hit = true;
+  this.emoji = randomizeEmoji();
 
 	this.place = function(objArray){
 
 			for(i=0;i<objArray.length;i++){
-				if(this.id != i){ //dont do the check if it is looking at itself
-
-					this.hit = collideCircleCircle(this.x, this.y, this.d, objArray[i].x, objArray[i].y, objArray[i].d); //colliding with anything?
-
-					if(this.hit == true){ // if we ever get a true we have to try again, this works since we iterate down through the objects one by one.
-						//try again:
+				if(this.id != i){
+					this.hit = collideCircleCircle(this.x, this.y, this.d, objArray[i].x, objArray[i].y, objArray[i].d);
+					if(this.hit == true){
 						this.x = random(width)
 						this.y = random(height)
 					}
@@ -61,7 +121,16 @@ function circObj(d, id){
 	this.disp = function(){
 		noStroke();
 		fill(this.color);
-		ellipse(this.x,this.y,this.d,this.d);
+    textSize(this.d)
+    // console.log(this.emoji)
+		text(this.emoji,this.x,this.y);
+    archive.push(this.emoji)
+    textSize(this.d)
+		text(this.emoji,this.x,this.y);
+    archive.push(this.emoji)
+    textSize(this.d)
+		text(this.emoji,this.x,this.y);
+    archive.push(this.emoji)
 
 	}
 
